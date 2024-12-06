f569e77519d93e36884bcc4d90a8afb2a21a209f
nexx14 <mnabilcf65@gmail.com>
2024-12-06 10:16:21 +0700

    Sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,101 +1,74 @@
 <!-- start sidebar -->
-<div id="sidebar" class="relative flex flex-col flex-wrap bg-white border-r border-gray-300 p-6 flex-none w-64 lg:ml-0 md:-ml-64 md:fixed md:top-0 md:z-30 md:h-screen md:shadow-xl animated faster">
+<div id="sidebar" class="relative flex flex-col flex-wrap bg-white border-r border-gray-300 p-6 flex-none w-64 lg:ml-0 md:-ml-64 md:fixed md:top-0 md:z-30 md:h-screen md:shadow-xl animated faster text-sm">
+    <!-- Semua ikon akan menggunakan ukuran text-sm -->
 
     <!-- sidebar content -->
-    <div class="flex flex-col fixed">
-
-        <!-- sidebar toggle -->
-        <div class="text-right hidden md:block mb-4">
-            {{-- <button id="sideBarHideBtn">
-                <i class="fad fa-times-circle"></i>
-            </button> --}}
-        </div>
-        <!-- end sidebar toggle -->
+    <div class="flex flex-col">
 
         <p class="uppercase text-xs text-gray-600 mb-4 tracking-wider">homes</p>
 
         <!-- link -->
         <a href="/dashboard"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-chart-pie text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-chart-pie class="mr-2 w-6 h-6" />
             Analytics dashboard
         </a>
-
-       
 
         <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Master Data</p>
 
         <!-- link -->
         <a href="/dashboard/category"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-envelope-open-text text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-folder-outline class="mr-2 w-6 h-6" />
             Kategori
         </a>
-        <!-- end link -->
         <!-- link -->
         <a href="/dashboard/category"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-envelope-open-text text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-format-list-bulleted class="mr-2 w-6 h-6" />
             Satuan
         </a>
-        <!-- end link -->
-
         <!-- link -->
         <a href="/dashboard/supplier"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-comments text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-account-multiple class="mr-2 w-6 h-6" />
             Supplier
         </a>
-        <!-- end link -->
-
         <!-- link -->
         <a href="/dashboard/barang"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-shield-check text-xs mr-2"></i>
-           Barang
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-cube-outline class="mr-2 w-6 h-6" />
+            Barang
         </a>
-        <!-- end link -->
-
-       
 
         <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">Transaksi</p>
 
         <!-- link -->
         <a href="./typography.html"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-text text-xs mr-2"></i>
-            Permintaaan barang
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-clipboard-check-outline class="mr-2 w-6 h-6" />
+            Permintaan Barang
         </a>
-        <!-- end link -->
-
         <!-- link -->
         <a href="./alert.html"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-whistle text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-truck-fast class="mr-2 w-6 h-6" />
             Barang Keluar
         </a>
-        <!-- end link -->
-
 
         <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">User Managemen</p>
         <!-- link -->
         <a href="./alert.html"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-whistle text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-shield-key-outline class="mr-2 w-6 h-6" />
             Akses / Permission
         </a>
-        <!-- end link -->
         <!-- link -->
         <a href="./alert.html"
-            class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
-            <i class="fad fa-whistle text-xs mr-2"></i>
+            class="mb-3 capitalize font-medium hover:text-teal-600 transition ease-in-out duration-500 flex items-center">
+            <x-mdi-account-key-outline class="mr-2 w-6 h-6" />
             Role
         </a>
-        <!-- end link -->
-
-        
-
     </div>
-
 </div>
-<div id="overlay" onclick="toggleSidebar()" class="fixed inset-0 bg-black/40 hidden" ></div>
+<div id="overlay" onclick="toggleSidebar()" class="fixed inset-0 bg-black/40 hidden"></div>
