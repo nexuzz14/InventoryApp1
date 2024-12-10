0a69756d0efddd7205164de19dee8033d46d4498
ThenmusteSatrio <gilangsatrio218@gmail.com>
2024-12-10 19:49:43 +0700

    kndkjshdgt

--- app/Http/Controllers/chartController.php
+++ app/Http/Controllers/chartController.php
@@ -10,79 +10,78 @@ use Illuminate\Support\Facades\Log;
 
 class chartController extends Controller
 {
-    public function chart(){
-        if(Auth::check()){
-          $data = chart::with('item')->where('user_id', Auth::user()->id)->get();
-  
-          return view('invoice', compact('data'));
-  
+    public function chart()
+    {
+        if (Auth::check()) {
+            $data = chart::with('item')->where('user_id', Auth::user()->id)->get();
+            return view('invoice', compact('data'));
         }
- }
- public function update($id, Request $request)
- {
-     try {
+    }
+    public function update($id, Request $request)
+    {
+        try {
             $data = $request->validate([
-                'quantity'=>'integer|required'
+                'quantity' => 'integer|required'
             ]);
-         if (Auth::check()) {
-             $idOrigin = Crypt::decrypt($id);
-             $data = Chart::find($idOrigin);
- 
-             if ($data && Auth::user()->id == $data['user_id']) {
-                 $newQuantity = $request->input('quantity', 0);
-                 $item = $data->item; 
-                 if ($newQuantity <= $item->quantity) {
-                     $data->quantity = $newQuantity;
-                     $data->save();
- 
-                     return redirect()->back();
-                 } else {
-                     return redirect()->back()->with("message", "Jumlah melebihi stok tersedia");
-                 }
-             } else {
-                 return redirect()->back()->with("message", "Data tidak valid");
-             }
-         } else {
-             return redirect()->back()->with("message", "Harap login terlebih dahulu");
-         }
-     } catch (\Exception $e) {
-        Log::alert($e);
-         return redirect()->back()->with("message", "Terjadi kesalahan: " . $e->getMessage());
-     }
- }
-    public function delete($id){
-        try{
-            if(Auth::check()){
+            if (Auth::check()) {
                 $idOrigin = Crypt::decrypt($id);
-                $data =  chart::find($idOrigin);
-                if($data && Auth::user()->id == $data['user_id']){
+                $data = Chart::find($idOrigin);
+
+                if ($data && Auth::user()->id == $data['user_id']) {
+                    $newQuantity = $request->input('quantity', 0);
+                    $item = $data->item;
+                    if ($newQuantity <= $item->quantity) {
+                        $data->quantity = $newQuantity;
+                        $data->save();
+
+                        return redirect()->back();
+                    } else {
+                        return redirect()->back()->with("message", "Jumlah melebihi stok tersedia");
+                    }
+                } else {
+                    return redirect()->back()->with("message", "Data tidak valid");
+                }
+            } else {
+                return redirect()->back()->with("message", "Harap login terlebih dahulu");
+            }
+        } catch (\Exception $e) {
+            Log::alert($e);
+            return redirect()->back()->with("message", "Terjadi kesalahan: " . $e->getMessage());
+        }
+    }
+    public function delete($id)
+    {
+        try {
+            if (Auth::check()) {
+                $idOrigin = Crypt::decrypt($id);
+                $data = chart::find($idOrigin);
+                if ($data && Auth::user()->id == $data['user_id']) {
                     $data->delete();
                     return redirect()->back()->with("message", "Barang dihapus Dari daftar");
 
-                }else{
+                } else {
                     return redirect()->back()->with("message", "Barang gagal dihapus Dari daftar");
                 }
             }
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             return redirect()->back()->with("message", "Barang gagal dihapus Dari daftar");
 
         }
-        
+
     }
 
-    public function store(Request $request){
-        try{
+    public function store(Request $request)
+    {
+        try {
             Log::alert($request);
-            //delete chart
-            foreach($request->chartData as $item){
+            foreach ($request->chartData as $item) {
                 $data = Chart::find($item);
-                if($data){
+                if ($data) {
                     $data->delete();
                 }
             }
             return redirect()->back()->with("message", "order berhasil");
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return redirect()->back()->with("message", "Order Gagal");
         }
     }
